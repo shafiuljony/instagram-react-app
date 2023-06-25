b4c2258cbe7a7d7ec58916c688e4f0358476c276
shafiuljony <shafiuljony12@gmail.com>
2023-06-25 15:27:33 +0600

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState,useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
+import Iphonewithapp from "../../public/images/iphone-with-profile.jpg";
+import Logo from "../../public/images/logo.png";
 
 export default function Login(){
     const navigate = useNavigate();
@@ -19,6 +21,49 @@ export default function Login(){
     },[])
 
     return(
-        <h1 className="underline text-3xl font-bold">I am the Login Page lets get started.</h1>
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <div className="flex w-3/5">
+                <img src={Iphonewithapp} alt="iphone with instagram app" />
+            </div>
+            <div className="flex flex-col w-2/5">
+                <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+                    <h1 className="flex justify-center w-full">
+                        <img src={Logo} alt="instagram" className="mt-2 w-6/12 mb-4" />
+                    </h1>
+                    {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+                    <form onSubmit={handleLogin} method="POST">
+                        <input 
+                            aria-label="Enter your email address"
+                            type="text"
+                            placeholder="Email address"   
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2" 
+                            onChange={({ target }) => setEmailAddress(target.value)}    
+                        />
+                        <input 
+                            aria-label="Enter your password address"
+                            type="password"
+                            placeholder="Password"   
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2" 
+                            onChange={({ target }) => setPassword(target.value)}    
+                        />
+                        <button 
+                            disabled={isInvalid}
+                            type="submit"
+                            className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}
+                        >
+                            Login
+                        </button>
+                    </form>        
+                </div>
+                <div className="flex justify-center items-center flex-col w-full bg-white border border-gray-primary">
+                    <p className="text-sm">Don't have an account?{` `}
+                        <Link to='signup' className="font-bold text-blue-medium">
+                            Sign up
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
