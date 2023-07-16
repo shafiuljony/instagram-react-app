efb778267cea93686df27675fbd5b618f44129b7
shafiuljony <shafiuljony12@gmail.com>
2023-07-16 15:29:00 +0600

    comments

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -4,6 +4,7 @@ import Header from "./Header"
 import Image from "./image"
 import Actions from "./Actions"
 import Footer from "./Footer"
+import Comments from "./Comments"
 
 export default function Post({content}){
     const commentInput = useRef(null);
@@ -15,10 +16,27 @@ export default function Post({content}){
 
     return (
         <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
-            <Header username={content.username} />
-            <Image src={content.imageSrc} caption={content.caption} />
-            <Actions docId={content.docId} totalLikes={content.likes.length} likedPhoto={content.userLikedPhoto} handleFocus={handleFocus} />
-            <Footer caption={content.caption} username={content.username} />
+            <Header 
+                username={content.username} 
+            />
+            <Image 
+                src={content.imageSrc} 
+                caption={content.caption} 
+            />
+            <Actions 
+                docId={content.docId} 
+                totalLikes={content.likes.length} likedPhoto={content.userLikedPhoto} handleFocus={handleFocus} 
+            />
+            <Footer 
+                caption={content.caption} 
+                username={content.username} 
+            />
+            <Comments 
+                docId={content.docId} 
+                comments={content.comments} 
+                posted={content.dateCreated} 
+                commentInput={commentInput} 
+            />
         </div>
     )
 }
