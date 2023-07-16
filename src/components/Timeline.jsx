b2ca145258114edb44723b858b03fdcfcbb6873f
shafiuljony <shafiuljony12@gmail.com>
2023-07-16 12:59:12 +0600

    timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,15 +1,24 @@
 import Skeleton from "react-loading-skeleton"
-
+import usePhotos from "../hooks/use-photos"
 
 export default function Timeline() {
     //we need to get the logged in user's photos (hook)
+    const { photos } = usePhotos();
     //on loading the photos, we need to use react skeleton
     //if we have photos, render them (create a post component)
     //if the user has no photos, tell them to create some photos
 
     return (
         <div className="container col-span-2">
-            <p>I am Timeline</p>
+            {!photos ? (
+                <>
+                    <Skeleton count={4} width={640} height={500} className="mb-5" />
+                </>    
+            ) : photos?.length > 0 ? (
+                photos.map((content)=> <p key={content.docId}>{content.imageSrc}</p>)
+            ): (
+                <p className="text-center text-2xl">Follow people to see photos!</p>
+            )}
         </div>
     )
 }
--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -3,25 +3,36 @@ import UserContext from "../context/user";
 import { getPhotos, getUserByUserId } from "../services/Firebase";
 
 export default function usePhotos() {
+
     const [photos, setPhotos] = useState(null);
+
     const {
-        user: { uid: userId = "" }
+        user: { uid: userId = '' }
     } = useContext(UserContext);
 
+    // const { user } = useContext(UserContext);
+    // const userId = user ? user.uid :'';
+
+// console.log('userId', userId);
     useEffect(() => {
         async function getTimelinePhotos() {
+
             //example: [2,1,5] <- 2 being raphel
-            const followingUserIds = await getUserByUserId(userId)
+            const [{following}] = await getUserByUserId(userId)
             let followedUserPhotos = [];
 
             // does the user actually follow people?
             if(following.length > 0){
-                followedUserPhotos = await getPhotos(userId, followingUserIds);
+                followedUserPhotos = await getPhotos(userId, following);
             }
 
+            // re-arrange array to be newest photos first by dateCreated
+            followedUserPhotos.sort((a,b)=> b.dateCreated - a.dateCreated);
+            setPhotos(followedUserPhotos);
         }
+        // console.log(userId);
+        getTimelinePhotos();
+    },[userId]);
 
-        console.log(userId);
-        // getTimelinePhotos();
-    },[]);
+    return { photos };
 }
